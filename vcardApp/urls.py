5473a06453050ecaf13abfde733d77002610fdf1
himangshuishere <dehimangshu2020@gmail.com>
2022-04-04 20:33:55 +0530

    Refactor Done
    Added routes in the urls.py

--- vcardApp/urls.py
+++ vcardApp/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 
+from vcardApp.views import CategoryAPIView, ProductAPIView
+
 urlpatterns = [
-    
+    path('', CategoryAPIView.as_view(), name='categories'),
+    path('products/', ProductAPIView.as_view(), name='products'),
 ]
